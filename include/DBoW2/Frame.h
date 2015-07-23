6f83a2e2e9c8d9e75e2cbea72b9c0dfc6360d001
mou <mou@sheldon.(none)>
2015-07-23 21:02:31 +0800

    add class Frame

---
+++ include/DBoW2/Frame.h
@@ -0,0 +1,53 @@
+/**
+ * File: Frame.h
+ * Date: November 2011
+ * Author: Wei Mou
+ * Description: Frame class
+ * License: see the LICENSE.txt file
+ *
+ */
+
+#ifndef __D_T_FRAME_H__
+#define __D_T_FRAME_H__
+
+#include <opencv2/core/core.hpp>
+#include <vector>
+#include <string>
+#include <opencv2/line_descriptor/descriptor.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+namespace DBoW2 {
+
+    enum DETECTOR_TYPE {
+        LINE,
+        ORB,
+        LINEORB
+    };
+    class Frame
+    {
+        public:
+            Frame();
+            Frame(std::string imgFileName, DETECTOR_TYPE detectorType = LINEORB);
+            Frame(std::string imgFileName, int id);
+            void detectLineFeatures();
+            void detectOrbFeatures();
+            void loadFromFile(std::string filename);
+            void writeToFile(std::string filename);
+        private:
+            std::vector<cv::Mat> m_orbFeatureDescs;
+            std::vector<cv::Mat> m_lineFeatureDescs;
+            std::vector<cv::KeyPoint> m_keyPts;
+            std::vector<cv::line_descriptor::KeyLine> m_keyLines;
+            std::string m_imageName;
+            std::vector<uchar> m_imgDescOrb;
+            std::vector<uchar> m_imgDescLine;
+            cv::Mat m_img;
+            std::vector<double> m_pose;
+            int m_id;
+            int m_width;
+            int m_height;
+            DETECTOR_TYPE m_detectorType;
+    };
+} // namespace DBoW2
+#endif
+
---
+++ src/Frame.cpp
@@ -0,0 +1,88 @@
+#include <Frame.h>
+
+using namespace cv;
+using namespace cv::line_descriptor;
+using namespace DBoW2;
+Frame::Frame() {
+}
+Frame::Frame(std::string imgFileName, int id) {
+    m_imageName = imgFileName;
+    m_id = id;
+    m_img = imread(imgFileName, 0);
+    if(m_img.empty()) {
+        printf("load image %s fail\n", m_imageName.c_str());
+    }
+    m_width = m_img.cols;
+    m_height = m_img.rows;
+}
+
+void Frame::detectLineFeatures() {
+    if(m_img.empty()) {
+        printf("image %d empty, skip line detection\n", m_id);
+        return;
+    }
+    Ptr<BinaryDescriptor> lbd = BinaryDescriptor::createBinaryDescriptor();
+    lbd->detect(m_img, m_keyLines/*, mask0*/);
+    cv::Mat lineDescriptors;
+    lbd->compute(m_img, m_keyLines, lineDescriptors);
+    if(lineDescriptors.rows < 50) {
+        printf("image %d: number of line features low: %d features\n", m_id, lineDescriptors.rows);
+    }
+    m_lineFeatureDescs.resize(lineDescriptors.rows);
+    for(size_t j = 0; j < m_lineFeatureDescs.size(); ++j) {
+        m_lineFeatureDescs[j] = lineDescriptors.row(j);
+    }
+}
+
+void Frame::detectOrbFeatures() {
+    if(m_img.empty()) {
+        printf("image %d empty, skip orb detection\n", m_id);
+        return;
+    }
+    //Ptr<BinaryDescriptor> lbd = BinaryDescriptor::createBinaryDescriptor();
+    cv::Ptr<cv::ORB> orb = cv::ORB::create(800, 1.2, 4);
+    cv::Mat orbDescriptors;
+    orb->detectAndCompute(m_img, cv::Mat(), m_keyPts, orbDescriptors, false);
+    if(orbDescriptors.rows < 100) {
+        printf("image %d: number of orb features low: %d features\n", m_id, orbDescriptors.rows);
+    }
+    m_orbFeatureDescs.resize(orbDescriptors.rows);
+    for(size_t j = 0; j < m_orbFeatureDescs.size(); ++j) {
+        m_orbFeatureDescs[j] = orbDescriptors.row(j);
+    }
+}
+
+void Frame::writeToFile(std::string outFileName) {
+    //Write serialization for this class
+    FileStorage fs(outFileName, FileStorage::WRITE);
+    fs << "f_id" << m_id;
+    fs << "f_width" << m_width;
+    fs << "f_height" << m_height;
+    fs << "f_imageName" << m_imageName;
+    fs << "f_pose" << m_pose;
+    //fs << "f_keyPts" << m_keyPts;
+    //fs << "f_keyLines" << m_keyLines; //no matching call for this
+    fs << "f_orbFeatureDescs" << m_orbFeatureDescs;
+    fs << "f_lineFeatureDescs" << m_lineFeatureDescs;
+    fs.release();
+}
+
+void Frame::loadFromFile(std::string inFileName) {
+    FileStorage fs(inFileName, FileStorage::READ);
+    if (!fs.isOpened())
+    {
+        std::cerr << "Failed to open " << inFileName << std::endl;
+        return;
+    }
+    m_id = (int) fs["f_id"];
+    m_width = (int)fs["f_width"];
+    m_height = (int)fs["f_height"];
+    m_imageName = (std::string)fs["f_imageName"];
+    FileNode fileNode = fs["f_pose"];
+    cv::read(fileNode, m_pose);
+    fileNode = fs["f_orbFeatureDescs"];
+    cv::read(fileNode, m_orbFeatureDescs);
+    fileNode = fs["f_lineFeatureDescs"];
+    cv::read(fileNode, m_lineFeatureDescs);
+    fs.release();
+}
